88123d3e2fe7b5c14913e1927067fd5092ed9a6a
christophberger <github@christophberger.com>
2024-06-28 19:15:47 +0200

    Brew formula update for goman version v0.3.1

--- goman.rb
+++ goman.rb
@@ -11,7 +11,7 @@ class Goman < Formula
   on_macos do
     on_intel do
       url "https://github.com/appliedgocode/goman/releases/download/v0.3.1/goman_0.3.1_darwin_amd64.tar.gz"
-      sha256 "1d50b918bc2d5f36d8895f548cfa0984d425a57dd2706a4c63d3645d444cf852"
+      sha256 "061b7477457e32315fdf2cff042924ebc969106bce37f25491aadd6c8270073c"
 
       def install
         bin.install "goman"
@@ -19,7 +19,7 @@ class Goman < Formula
     end
     on_arm do
       url "https://github.com/appliedgocode/goman/releases/download/v0.3.1/goman_0.3.1_darwin_arm64.tar.gz"
-      sha256 "1600d307073a07f5ebbd115e12041277359a4587a349427e0450ff741a761d87"
+      sha256 "fcc217dfae82d735626caf3bdb62eb3344e19aa255a15bee7907744bf9c13de1"
 
       def install
         bin.install "goman"
@@ -31,7 +31,7 @@ class Goman < Formula
     on_intel do
       if Hardware::CPU.is_64_bit?
         url "https://github.com/appliedgocode/goman/releases/download/v0.3.1/goman_0.3.1_linux_amd64.tar.gz"
-        sha256 "1c37ff9d510a9bfe360aca210e1b010343a3b9bc59f816aed6b12aebaff5e79d"
+        sha256 "2efe035656f059527442eb12e7ed9ab58f9564ccb1379ea4d69229c1f082614c"
 
         def install
           bin.install "goman"
@@ -41,7 +41,7 @@ class Goman < Formula
     on_arm do
       if Hardware::CPU.is_64_bit?
         url "https://github.com/appliedgocode/goman/releases/download/v0.3.1/goman_0.3.1_linux_arm64.tar.gz"
-        sha256 "bd99735ae49ecdf646628ccf714e4faf87891a98525ce037c6e8899a991bde46"
+        sha256 "8d09614694ef56a91024fe73a717bc103b68b680d28ab38de194d516e8518c7b"
 
         def install
           bin.install "goman"
